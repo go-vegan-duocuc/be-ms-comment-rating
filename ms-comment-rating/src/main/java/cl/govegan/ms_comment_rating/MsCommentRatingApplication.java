a4fb623fbd557876dc84c97f8e2ba4b2082707c7
Gab-Serrano <gab.serrano@duocuc.cl>
2024-06-05 23:06:15 -0400

    # init repo

---
+++ ms-comment-rating/src/main/java/cl/govegan/ms_comment_rating/MsCommentRatingApplication.java
@@ -0,0 +1,13 @@
+package cl.govegan.ms_comment_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCommentRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCommentRatingApplication.class, args);
+	}
+
+}
---
+++ ms-comment-rating/src/test/java/cl/govegan/ms_comment_rating/MsCommentRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.govegan.ms_comment_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCommentRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
