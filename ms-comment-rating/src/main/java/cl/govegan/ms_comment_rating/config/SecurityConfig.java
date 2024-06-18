a9fd47c71f87499f2c38ed213e78dfb52ee1b48d
pipussdipuss <fel.iturrieta@duocuc.cl>
2024-06-18 00:46:56 -0400

    se ajustó la seguidad para CORS de los endpoints

--- ms-comment-rating/src/main/java/cl/govegan/ms_comment_rating/config/SecurityConfig.java
+++ ms-comment-rating/src/main/java/cl/govegan/ms_comment_rating/config/SecurityConfig.java
@@ -1,20 +1,45 @@
 package cl.govegan.ms_comment_rating.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
+@EnableWebMvc
 public class SecurityConfig implements WebMvcConfigurer{
     
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) { 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8100", "http://localhost:8082")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+        .allowedOrigins("http://localhost:8100", "http://localhost:8082")
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+        .allowedHeaders("*").allowCredentials(true);
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/api/v1/comments/status").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/ratings/status").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/comments").permitAll()
+                .requestMatchers(HttpMethod.POST, "/api/v1/ratings/add").permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/comments/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/comments/**").authenticated()
+                .requestMatchers(HttpMethod.PUT, "/api/v1/comments/**").authenticated()
+                .requestMatchers(HttpMethod.GET, "/api/v1/ratings/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/api/v1/ratings/**").authenticated()
+                .requestMatchers(HttpMethod.PUT, "/api/v1/ratings/**").authenticated()
+                .anyRequest().authenticated()
+            );
+        return http.build();
 
+            
+    }
+    
 }
